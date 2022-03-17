53e8b5475798a91ea4692d82cf2e141690250a51
valentina2708 <75132700+valentina2708@users.noreply.github.com>
2022-03-16 22:08:40 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2_Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Enum
+{
+    internal class Program
+    {
+
+        enum zoologico {aves,salvajes,mar,domesticos};
+        static void Main(string[] args)
+        {
+            zoologico animales = zoologico.mar;
+
+            if (animales == zoologico.aves)
+            {
+                Console.WriteLine("El zoologico tiene aves");
+            }if (animales == zoologico.salvajes)
+            {
+                Console.WriteLine("El zoologico tiene animales salvajes");
+            }
+            if (animales == zoologico.domesticos)
+            {
+                Console.WriteLine("El zoologico tiene animales Domesticos");
+            }
+            if (animales == zoologico.mar)
+            {
+                Console.WriteLine("El zoologico tiene animales de mar");
+            }
+
+
+            Console.ReadKey();
+
+
+        } 
+
+    }
+}
